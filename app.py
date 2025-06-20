96a04fc1a16e726642b358e15a049c587aab386e
SumanthG9 <166027298+SumanthG9@users.noreply.github.com>
2025-06-20 17:06:39 +0530

    added sqlite database

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask,render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db =SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content =db.Column(db.String(250), nullable=False)
+    completed =db.Column(db.Boolean, default=False)
+    date = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return "<Task %r>" %self.id
 
 @app.route('/')
 def index():
