11c087c2a267bc5eff5088b20dc029a401a4bb4b
SumanthG9 <166027298+SumanthG9@users.noreply.github.com>
2025-06-20 15:30:28 +0530

    added html file and render template import

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "test case 1"
+    return render_template("index.html")
 
 if __name__ =="__main__":
     app.run(debug=True)
