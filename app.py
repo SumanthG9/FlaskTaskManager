6648eec621e106bb89731a8009044ef2895e3f72
SumanthG9 <166027298+SumanthG9@users.noreply.github.com>
2025-06-20 15:22:44 +0530

    corrected it

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return index.html
+    return "test case 1"
 
-if __name__ =='__main__':
+if __name__ =="__main__":
     app.run(debug=True)
